590fb597fc66f20ee5d8297c231c1fa548475fd5
Affan Showkat Hakak <affanshowkat@gmail.com>
2022-02-04 11:08:03 +0530

    Added async and await examples

--- async.js
+++ async.js
@@ -1,260 +1,366 @@
-//call stack example.
+// //call stack example.
 
-const multiply = (x, y) => x * y;
+// const multiply = (x, y) => x * y;
 
-const square = (x) => multiply(x, x);
+// const square = (x) => multiply(x, x);
 
-const isRightTriangle = (a, b, c) =>
-  square(a) + square(b) === square(c);
+// const isRightTriangle = (a, b, c) =>
+//   square(a) + square(b) === square(c);
 
-isRightTriangle(3, 4, 5);
+// isRightTriangle(3, 4, 5);
 
-//call back hell.
+// //call back hell.
 
-setTimeout(() => {
-  document.body.style.backgroundColor =
-    "violet";
-}, 1000);
+// setTimeout(() => {
+//   document.body.style.backgroundColor =
+//     "violet";
+// }, 1000);
 
-setTimeout(() => {
-  document.body.style.backgroundColor =
-    "indigo";
-}, 2000);
+// setTimeout(() => {
+//   document.body.style.backgroundColor =
+//     "indigo";
+// }, 2000);
 
-setTimeout(() => {
-  document.body.style.backgroundColor =
-    "blue";
-}, 3000);
+// setTimeout(() => {
+//   document.body.style.backgroundColor =
+//     "blue";
+// }, 3000);
 
-setTimeout(() => {
-  document.body.style.backgroundColor =
-    "green";
-}, 4000);
+// setTimeout(() => {
+//   document.body.style.backgroundColor =
+//     "green";
+// }, 4000);
 
-setTimeout(() => {
-  document.body.style.backgroundColor =
-    "yellow";
-}, 5000);
+// setTimeout(() => {
+//   document.body.style.backgroundColor =
+//     "yellow";
+// }, 5000);
 
-setTimeout(() => {
-  document.body.style.backgroundColor =
-    "orange";
-}, 6000);
+// setTimeout(() => {
+//   document.body.style.backgroundColor =
+//     "orange";
+// }, 6000);
 
-setTimeout(() => {
-  document.body.style.backgroundColor =
-    "red";
-}, 7000);
+// setTimeout(() => {
+//   document.body.style.backgroundColor =
+//     "red";
+// }, 7000);
 
-//nesting so as to keep timeout same.
+// //nesting so as to keep timeout same.
 
-setTimeout(() => {
-  document.body.style.backgroundColor =
-    "violet";
-  setTimeout(() => {
-    document.body.style.backgroundColor =
-      "indigo";
-    setTimeout(() => {
-      document.body.style.backgroundColor =
-        "blue";
-      setTimeout(() => {
-        document.body.style.backgroundColor =
-          "green";
-        setTimeout(() => {
-          document.body.style.backgroundColor =
-            "yellow";
-          setTimeout(() => {
-            document.body.style.backgroundColor =
-              "orange";
-            setTimeout(() => {
-              document.body.style.backgroundColor =
-                "red";
-            }, 1000);
-          }, 1000);
-        }, 1000);
-      }, 1000);
-    }, 1000);
-  }, 1000);
-}, 1000);
+// setTimeout(() => {
+//   document.body.style.backgroundColor =
+//     "violet";
+//   setTimeout(() => {
+//     document.body.style.backgroundColor =
+//       "indigo";
+//     setTimeout(() => {
+//       document.body.style.backgroundColor =
+//         "blue";
+//       setTimeout(() => {
+//         document.body.style.backgroundColor =
+//           "green";
+//         setTimeout(() => {
+//           document.body.style.backgroundColor =
+//             "yellow";
+//           setTimeout(() => {
+//             document.body.style.backgroundColor =
+//               "orange";
+//             setTimeout(() => {
+//               document.body.style.backgroundColor =
+//                 "red";
+//             }, 1000);
+//           }, 1000);
+//         }, 1000);
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
 
-//we can write a function if we are gonna reuse it and if we are gonna change delays.
+// //we can write a function if we are gonna reuse it and if we are gonna change delays.
 
-const delayedColorChange = (
-  newColor,
-  delay,
-  doNext //doNext is a callback function to give an equal delay time to our function.
-) => {
-  setTimeout(() => {
-    document.body.style.backgroundColor =
-      newColor;
-    doNext();
-  }, delay);
-};
+// const delayedColorChange = (
+//   newColor,
+//   delay,
+//   doNext //doNext is a callback function to give an equal delay time to our function.
+// ) => {
+//   setTimeout(() => {
+//     document.body.style.backgroundColor =
+//       newColor;
+//     doNext();
+//   }, delay);
+// };
 
-delayedColorChange(
-  "violet",
-  1000,
-  () => {
-    delayedColorChange(
-      "indigo",
-      1000,
-      () => {
-        delayedColorChange(
-          "blue",
-          1000,
-          () => {
-            delayedColorChange(
-              "green",
-              1000,
-              () => {
-                delayedColorChange(
-                  "yellow",
-                  1000,
-                  () => {
-                    delayedColorChange(
-                      "orange",
-                      1000,
-                      () => {
-                        delayedColorChange(
-                          "red",
-                          1000,
-                          () => {}
-                        );
-                      }
-                    );
-                  }
-                );
-              }
-            );
-          }
-        );
-      }
-    );
-  }
-);
+// delayedColorChange(
+//   "violet",
+//   1000,
+//   () => {
+//     delayedColorChange(
+//       "indigo",
+//       1000,
+//       () => {
+//         delayedColorChange(
+//           "blue",
+//           1000,
+//           () => {
+//             delayedColorChange(
+//               "green",
+//               1000,
+//               () => {
+//                 delayedColorChange(
+//                   "yellow",
+//                   1000,
+//                   () => {
+//                     delayedColorChange(
+//                       "orange",
+//                       1000,
+//                       () => {
+//                         delayedColorChange(
+//                           "red",
+//                           1000,
+//                           () => {}
+//                         );
+//                       }
+//                     );
+//                   }
+//                 );
+//               }
+//             );
+//           }
+//         );
+//       }
+//     );
+//   }
+// );
 
-//example of call back hell.
+// //example of call back hell.
 
-searchGamesAPI(
-  "COD",
-  () => {
-    saveToPS(
-      games,
-      () => {
-        console.log("something");
-      },
-      () => {
-        console.log("something again");
-      }
-    );
-  },
-  () => {
-    console.log("API disabled");
-  }
-);
+// searchGamesAPI(
+//   "COD",
+//   () => {
+//     saveToPS(
+//       games,
+//       () => {
+//         console.log("something");
+//       },
+//       () => {
+//         console.log("something again");
+//       }
+//     );
+//   },
+//   () => {
+//     console.log("API disabled");
+//   }
+// );
 
-//promises : A promise is an object representing the eventual completion or failure of an asynchronous operation.
-//we dont have to pass callbacks in promises, just a url.
-//sending fake requests via promises.
+// //promises : A promise is an object representing the eventual completion or failure of an asynchronous operation.
+// //we dont have to pass callbacks in promises, just a url.
+// //sending fake requests via promises.
 
-fakeRequestPromise(
-  "shop.com/api/pizza/page1"
-)
-  .then(() => {
-    console.log(
-      "it worked/promise resolved"
-    );
-    fakeRequestPromise(
-      "shop.com/api/pizza/page2"
-    )
-      .then(() => {
-        console.log(
-          "it worked again/promise resolved again"
-        );
-      })
-      .catch(() => {
-        console.log(
-          "it failed again/promise rejected again"
-        );
-      });
-  })
-  .catch(() => {
-    console.log(
-      "it failed/promise rejected"
-    );
-  });
+// fakeRequestPromise(
+//   "shop.com/api/pizza/page1"
+// )
+//   .then(() => {
+//     console.log(
+//       "it worked/promise resolved"
+//     );
+//     fakeRequestPromise(
+//       "shop.com/api/pizza/page2"
+//     )
+//       .then(() => {
+//         console.log(
+//           "it worked again/promise resolved again"
+//         );
+//       })
+//       .catch(() => {
+//         console.log(
+//           "it failed again/promise rejected again"
+//         );
+//       });
+//   })
+//   .catch(() => {
+//     console.log(
+//       "it failed/promise rejected"
+//     );
+//   });
 
-// making promises better by rewriting .then's (promises are magic)
+// // making promises better by rewriting .then's (promises are magic)
 
-fakeRequestPromise(
-  "shop.com/api/pizza/page1"
-)
-  .then((data) => {
-    console.log("page 1 is on");
-    console.log(data);
-    return fakeRequestPromise(
-      "shop.com/api/pizza/page2"
-    );
-  })
-  .then((data) => {
-    console.log("page 2 is on");
-    console.log(data);
-    return fakeRequestPromise(
-      "shop.com/api/pizza/page3"
-    );
-  })
-  .then((data) => {
-    console.log("page 3 is on");
-    console.log(data);
-  })
-  .catch(() => {
-    console.log("pages not loading");
-    console.log(error);
-  });
+// fakeRequestPromise(
+//   "shop.com/api/pizza/page1"
+// )
+//   .then((data) => {
+//     console.log("page 1 is on");
+//     console.log(data);
+//     return fakeRequestPromise(
+//       "shop.com/api/pizza/page2"
+//     );
+//   })
+//   .then((data) => {
+//     console.log("page 2 is on");
+//     console.log(data);
+//     return fakeRequestPromise(
+//       "shop.com/api/pizza/page3"
+//     );
+//   })
+//   .then((data) => {
+//     console.log("page 3 is on");
+//     console.log(data);
+//   })
+//   .catch(() => {
+//     console.log("pages not loading");
+//     console.log(error);
+//   });
 
-//creating our own promises
+// //creating our own promises
 
-const fakeRequest = (url) => {
-  return new Promise(
-    (resolve, reject) => {
-      setTimeout(() => {
-        resolve();
-      }, 1000);
-    }
-  );
-};
+// const fakeRequest = (url) => {
+//   return new Promise(
+//     (resolve, reject) => {
+//       setTimeout(() => {
+//         resolve();
+//       }, 1000);
+//     }
+//   );
+// };
 
-fakeRequest("/pizza/1").then(() => {
-  console.log("Ok!! done");
-});
+// fakeRequest("/pizza/1").then(() => {
+//   console.log("Ok!! done");
+// });
 
-// example 2
+// // example 2
 
-const fakeRequest = (url) => {
-  return new Promise(
-    (resolve, reject) => {
-      const rand = Math.random();
-      setTimeout(() => {
-        if (rand < 0.7) {
-          resolve("fake data is here");
-        } else {
-          reject("request error");
-        }
-      }, 1000);
-    }
-  );
-};
+// const fakeRequest = (url) => {
+//   return new Promise(
+//     (resolve, reject) => {
+//       const rand = Math.random();
+//       setTimeout(() => {
+//         if (rand < 0.7) {
+//           resolve("fake data is here");
+//         } else {
+//           reject("request error");
+//         }
+//       }, 1000);
+//     }
+//   );
+// };
 
-fakeRequest("/pizza/1")
-  .then((data) => {
-    console.log("Ok!! done");
-    console.log("data is:", data);
-  })
-  .catch((err) => {
-    console.log("stop!", err);
-  });
+// fakeRequest("/pizza/1")
+//   .then((data) => {
+//     console.log("Ok!! done");
+//     console.log("data is:", data);
+//   })
+//   .catch((err) => {
+//     console.log("stop!", err);
+//   });
 
-//example 3
+// //example 3
+
+// const delayedColorChange = (
+//   color,
+//   delay
+// ) => {
+//   return new Promise(
+//     (resolve, reject) => {
+//       setTimeout(() => {
+//         document.body.style.backgroundColor =
+//           color;
+//         resolve();
+//       }, delay);
+//     }
+//   );
+// };
+
+// delayedColorChange("violet", 1000)
+//   .then(() => {
+//     return delayedColorChange(
+//       "indigo",
+//       1000
+//     );
+//   })
+//   .then(() => {
+//     return delayedColorChange(
+//       "blue",
+//       1000
+//     );
+//   })
+//   .then(() => {
+//     return delayedColorChange(
+//       "green",
+//       1000
+//     );
+//   })
+//   .then(() => {
+//     return delayedColorChange(
+//       "yellow",
+//       1000
+//     );
+//   })
+//   .then(() => {
+//     return delayedColorChange(
+//       "orange",
+//       1000
+//     );
+//   })
+//   .then(() => {
+//     return delayedColorChange(
+//       "red",
+//       1000
+//     );
+//   });
+
+// //async keyword allows us to write really asynchronous code or the syntax makeup for promises
+// // async functions return a promise
+// // if the function returns a value, the promise will be resolved with that value
+// // if the function throws an exception, the promise will be rejected
+
+// async function hello() {}
+
+// // example
+
+// const sing = async () => {
+//   throw "error is here";
+//   return "cold november rain";
+// };
+
+// sing()
+//   .then((data) => {
+//     console.log("resolved:", data);
+//   })
+//   .catch((err) => {
+//     console.log("rejected promise");
+//     console.log(err);
+//   });
+
+// //example
+
+// const login = async (
+//   username,
+//   password
+// ) => {
+//   if (!username || !password)
+//     throw "Credentials are missing";
+//   if (password === "heyyouarecute")
+//     return "yayyy!!!!";
+//   throw "Invalid";
+// };
+
+// login("coolnicepeople", "heyyouarecute")
+//   .then((msg) => {
+//     console.log("LOGGED IN");
+//     console.log(msg);
+//   })
+//   .catch((err) => {
+//     console.log("not valid");
+//     console.log(err);
+//   });
+
+// await keyword
+
+// await can only be used inside functions declared with async
+// await pauses the execution of the function, until promise resolves
+
+// example
 
 const delayedColorChange = (
   color,
@@ -271,42 +377,35 @@ const delayedColorChange = (
   );
 };
 
-delayedColorChange("violet", 1000)
-  .then(() => {
-    return delayedColorChange(
-      "indigo",
-      1000
-    );
-  })
-  .then(() => {
-    return delayedColorChange(
-      "blue",
-      1000
-    );
-  })
-  .then(() => {
-    return delayedColorChange(
-      "green",
-      1000
-    );
-  })
-  .then(() => {
-    return delayedColorChange(
-      "yellow",
-      1000
-    );
-  })
-  .then(() => {
-    return delayedColorChange(
-      "orange",
-      1000
-    );
-  })
-  .then(() => {
-    return delayedColorChange(
-      "red",
-      1000
-    );
-  });
+async function rainbow() {
+  await delayedColorChange(
+    "violet",
+    1000
+  );
+  await delayedColorChange(
+    "indigo",
+    1000
+  );
+  await delayedColorChange(
+    "blue",
+    1000
+  );
+  await delayedColorChange(
+    "green",
+    1000
+  );
+  await delayedColorChange(
+    "yellow",
+    1000
+  );
+  await delayedColorChange(
+    "orange",
+    1000
+  );
+  await delayedColorChange("red", 1000);
+  return "Rainbow is done";
+}
 
-//async keyword
+rainbow().then(() =>
+  console.log("Rainbow is done")
+);
